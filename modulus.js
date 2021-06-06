8b1f2eb8edb5a82bd11b79b4de242c8448dcfb76
Jia Liang Lee <jlee48@qub.ac.uk>
2021-06-06 18:03:24 +0100

    all functions

---
+++ modulus.js
@@ -0,0 +1,41 @@
+exports.handler = async (event) => {
+    
+    const Answer = {
+        "error" : "",
+        "string" : "",
+        "answer" : ""
+    };
+    
+    if(event.queryStringParameters){
+        var x = event.queryStringParameters.x;
+        var y = event.queryStringParameters.y;
+        
+        if((parseInt(x)) != x || (parseInt(y)) != y) {
+            Answer.error = true,
+            Answer.string = "Please provide a numeric value",
+            Answer.answer = 0
+        } else {
+            Answer.error = false,
+            Answer.string = x + "%" + y + "=" + ((parseInt(x)) % (parseInt(y))),
+            Answer.answer = (parseInt(x)) % (parseInt(y))
+        }
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(Answer),
+        };
+        return response;
+    } else {
+        Answer.error = true,
+        Answer.string = "Please provide a numeric value",
+        Answer.answer = 0
+        
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(Answer),
+        };
+        return response;
+    }
+    
+    
+};
---
+++ square.js
@@ -0,0 +1,41 @@
+exports.handler = async (event) => {
+    
+    const Answer = {
+        "error" : "",
+        "string" : "",
+        "answer" : ""
+    };
+    
+    if(event.queryStringParameters){
+        var x = event.queryStringParameters.x;
+        var y = event.queryStringParameters.y;
+        
+        if((parseInt(x)) != x) {
+            Answer.error = true,
+            Answer.string = "Please provide a numeric value",
+            Answer.answer = 0
+        } else {
+            Answer.error = false,
+            Answer.string = x + "*" + x + "=" + ((parseInt(x)) * (parseInt(x))),
+            Answer.answer = (parseInt(x)) * (parseInt(x))
+        }
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(Answer),
+        };
+        return response;
+    } else {
+        Answer.error = true,
+        Answer.string = "Please provide a numeric value",
+        Answer.answer = 0
+        
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(Answer),
+        };
+        return response;
+    }
+    
+    
+};
